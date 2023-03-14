2656549f1d20aab8c620e1cd2dc68432c26b2f72
Sreya <haldersreya@gmail.com>
2023-03-13 22:54:22 -0700

    debug

--- model.py
+++ model.py
@@ -26,20 +26,16 @@ class PandemicMDP:
         self.player_cards = []
         self.outbreak_count = 0
         self.game_over = False
-        self.m = 10
+        self.m = 20
         self.d = 2 #depth of rollouts
         self.c = 100 #exploration param
-        self.Q = {}
-        self.N = {}
         self.discount = 0.9
 
+        # START OF GAME
         random.shuffle(self.draw_pile)
         random.shuffle(self.infect_pile)
-        # Player starts the game drawing 4 cards
         for _ in range(4): self.player_cards.append(self.draw_pile.pop())
-        # Game starts with k cities infected
-        for _ in range(self.k):
-            self._infect(1)
+        for _ in range(self.k): self._infect(1)
     
     def _get_neighbors(self, city):
         return [n for n in self.map.neighbors(world.City(city).name)]
@@ -77,7 +73,6 @@ class PandemicMDP:
                     outbreak_cities.append(n)
             return False
         
-        self.outbreak_count += 1
         add_neighbors(city)
         for c in outbreak_cities:
             if self.disease_counts[c] >= 3:
@@ -99,10 +94,12 @@ class PandemicMDP:
         if self.disease_counts[self.current_city] > 0:
             if self.cure_status[color] == True:
                 self.disease_counts[self.current_city] = 0 
-                reward += 5
+                reward += 30
             else:
                 self.disease_counts[self.current_city] -= 1
-                reward += 1
+                reward += 5
+        else:
+            reward -= 30
     
     def build(self):
         if self.research_stations[self.current_city] == False and self.player_cards.count(self.current_city) > 0: 
@@ -117,7 +114,7 @@ class PandemicMDP:
                 for i in sorted(card_indices, reverse=True)[:4]:
                     del self.player_cards[i]
                 self.cure_status[color] = True
-                if all(self.cure_status.values()):
+                if all(self.cure_status):
                     reward += 100 # game ends when all cured
                     print('Congrats, you cured all the diseases!')
                     self.game_over = True
@@ -141,19 +138,27 @@ class PandemicMDP:
             self.build()
         elif action == "CURE":
             self.cure(reward)
+            if self.game_over: return reward
         else:
             raise ValueError("Invalid action")
 
-        for i in range(2):
+        print('INFECTING TWO CITIES:')
+        for _ in range(2):
+            if len(self.infect_pile) <= 0:
+                print('Ran out of infect cards, you lost.')
+                self.game_over = True
+                return reward
             self._infect(1)
+            if self.game_over:
+                return reward
 
         # Draw two cards from DRAW_PILE
-        for i in range(2):
-            card = self.draw_pile.pop()
+        for _ in range(2):
             if len(self.draw_pile) <= 0:
                 print('Ran out of draw cards, you lost.')
                 self.game_over = True
                 return reward
+            card = self.draw_pile.pop()
             # Check for epidemic
             if card == -1:
                 self._epidemic()
@@ -172,131 +177,58 @@ class PandemicMDP:
     def bonus(self, n_sum, n):
         return np.inf if n == 0 else math.sqrt(math.log(n_sum)/n)
 
-    def explore(self, s):
-            n_values = [self.N[(s, a)] for a in self.actions]
-            n_sum = sum(n_values)
-            ucb = np.array([self.Q[(s, a)] + self.c * self.bonus(n_sum, self.N[(s, a)]) for a in self.actions])
-            action_idx = np.argmax(ucb)
-            return self.actions[action_idx]
+    def explore(self, s, N, Q):
+        n_values = [N[(s, a)] for a in self.actions]
+        print(s, n_values)
+        n_sum = sum(n_values)
+        ucb = np.array([Q[(s, a)] + self.c * self.bonus(n_sum, N[(s, a)]) for a in self.actions])
+        print(ucb)
+        action_idx = np.argmax(ucb)
+        return self.actions[action_idx]
             
-    def simulate(self, s, d):
+    def simulate(self, s, d, N, Q):
+        print('Depth:', d)
         original_state = s
-        if self.d <= 0:
+        if d <= 0:
+            print('1')
             return 0
-        if not (s, self.actions[0]) in self.N.keys():
+        if not (s, self.actions[0]) in N.keys():
             for a in self.actions:
-                self.Q[(s, a)] = 0.0
-                self.N[(s, a)] = 0
+                Q[(s, a)] = 0.0
+                N[(s, a)] = 0
+            print('2')
+            # return 0
+        if self.game_over:
+            print('GAME OVER')
             return 0
-        a = self.explore(s)
-        r = self.step(a)
-        q = r + self.discount*self.simulate(tuple(self.disease_counts), d-1)
-        self.N[(original_state, a)] += 1
-        self.Q[(original_state, a)] += (q-self.Q[(original_state, a)]) / self.N[(original_state, a)]
+        rollout = copy.deepcopy(self)
+        a = rollout.explore(s, N, Q)
+        print('Take sim action:', a)
+        r = rollout.step(a)
+        q = r + self.discount * rollout.simulate(tuple(rollout.disease_counts), d-1, N, Q)
+        N[(original_state, a)] += 1
+        Q[(original_state, a)] += (q - Q[(original_state, a)]) / N[(original_state, a)]
         return q
-
-    """
-    def step(self, action):
-
-        if action == "MOVE":
-            neighbors = self._get_neighbors(self.current_city)
-            self.current_city = world.City[np.random.choice(neighbors)].value
-        elif action == "FLY":
-            new_city = self.player_cards[np.random.choice(len(self.player_cards))]
-            self.player_cards.remove(new_city)
-            self.current_city = new_city
-        #elif action == "CHARTER_FLIGHT":
-        #    if self.current_city in self.player_cards:
-        #        self.player_cards.remove(self.current_city)
-        #        self.current_city = self.player_cards[np.random.choice(len(self.player_cards))]
-        elif action == "TREAT":
-            if self.disease_counts[self.current_city] > 0:
-                if self.cure_status[color] == True:
-                    self.disease_counts[self.current_city] = 0 
-                    reward += 5
-                else:
-                    self.disease_counts[self.current_city] -= 1
-                    reward += 1
-        elif action == "BUILD":
-            if self.research_stations[self.current_city] == False and self.player_cards.count(self.current_city) > 0: #why do we check if player_cards count > 0
-                self.player_cards.remove(self.current_city)
-                self.research_stations[self.current_city] = True
-        elif action == "CURE":
-            color = self.color_map[self.current_city]
-            if self.cure_status[color] == False and self.research_stations[self.current_city] == True:
-                card_indices = [i for i in range(len(self.player_cards)) if self.color_map[self.player_cards[i]] == color]
-                if len(card_indices) >= 4:
-                    for i in sorted(card_indices, reverse=True)[:4]:
-                        del self.player_cards[i] #does this do in place deletion? change this to pop
-                    self.cure_status[color] = True
-                    if all(self.cure_status.values()):
-                        reward += 100 # game ends when all cured
-                        print('Congrats, you cured all the diseases!')
-                        self.game_over = True
-                    else:
-                        reward += 10
-            
-        else:
-            raise ValueError("Invalid action")
-
-        # Draw two cards from INFECT_PILE
-        for i in range(2):
-            self._infect(1)
-
-        # Draw two cards from DRAW_PILE
-        for i in range(2):
-            card = self.draw_pile.pop()
-            if len(self.draw_pile) == 0:
-                print('Ran out of draw cards, you lost.')
-                self.game_over = True
-            # Check for epidemic
-            if card == -1:
-                self._epidemic()
-                reward -= 50
-                continue
-            if len(self.player_cards) < 7:
-                self.player_cards.append(card)
-            else:
-                # Player must discard cards if they have too many
-                discard_indices = np.random.choice(7, 2, replace=False)
-                for index in sorted(discard_indices, reverse=True):
-                    del self.player_cards[index]
-
-        return self.current_city
-        """
-
-def play_game():
-    pandemic = PandemicMDP()
-    #actions = ["MOVE", "FLY", "TREAT", "BUILD", "CURE"]
-    print(f'Start of game. Start in ATLANTA. Current cards: {[world.City(i).name for i in pandemic.player_cards if i >= 0]}. Cure status: {pandemic.cure_status}')
-    while not pandemic.game_over:
-        a = random.choice(pandemic.actions)
-        print(f'----------------\nTake action {a}.')
-        print(f'Now in: {world.City(pandemic.step(a)).name}. Current cards: {[world.City(i).name for i in pandemic.player_cards if i >= 0]}')
-        disease_dict = {}
-        for i in range(len(pandemic.disease_counts)):
-            disease_dict[world.City(i).name] = pandemic.disease_counts[i]
-        print(f'Disease counts: {disease_dict}')
-    
-    print(f'GAME OVER. Cure status: {pandemic.cure_status}')
 
 def main():
     pandemic = PandemicMDP()
-    
+
+    N = {}
+    Q = {}
     while not pandemic.game_over:
         original_pandemic = copy.deepcopy(pandemic)
 
         print('------------------Simulate')
         for i in range(pandemic.m):
-            pandemic.simulate(tuple(pandemic.disease_counts), pandemic.d)
+            pandemic.simulate(tuple(pandemic.disease_counts), pandemic.d, N, Q)
         
         original_state = tuple(original_pandemic.disease_counts)
-        a = np.argmax(np.array([pandemic.Q[(original_state, a)] for a in pandemic.actions]))
+        a = np.argmax(np.array([Q[(original_state, a)] for a in pandemic.actions]))
         action = pandemic.actions[a]
         print(f'----------------Take action {action}.')
 
         pandemic = original_pandemic
-        pandemic.step(action)
+        print(pandemic.step(action))
 
     
 if __name__ == "__main__":
